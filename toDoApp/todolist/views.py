82be7e3ac702b7d25adc55c63131d90005f0fefe
Broncoespasmo <pe.estevez92@gmail.com>
2021-01-03 18:19:31 -0300

    se agrego tareas completas y borrado

--- toDoApp/todolist/views.py
+++ toDoApp/todolist/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import todoList
+from .forms import todoListForm
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 def index(request):
-	return render(request,'todolist/index.html')
+	todoItems = todoList.objects.order_by('id')
+	form = todoListForm()
+	context = {'todoItems':todoItems,'form':form}
+	return render(request,'todolist/index.html',context)
+
+@require_POST
+def addTodoItem(request):
+	form = todoListForm(request.POST)
+	if form.is_valid:
+		newTodoItem= todoList(text=request.POST['text'])
+		newTodoItem.save()
+	return redirect('index')
+
+def completedTodo(request,todoId):
+	toDo = todoList.objects.get(pk=todoId)
+	toDo.completed = True
+	toDo.save()
+	return redirect('index')
+
+def deleteCompleted(request):
+	todoList.objects.filter(completed__exact=True).delete()
+	return redirect('index')
+
+def deleteAll(request):
+	todoList.objects.all().delete()
+	return redirect('index')
